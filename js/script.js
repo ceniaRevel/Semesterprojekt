bfdaabcceaa75a52f5c6e362046cca73194494af
ceniaRevel <cenia.revel@stud.fhgr.ch>
2026-05-05 15:01:10 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("blub");
+async function loadData() {
+    const url = 'https://www.zuerich.com/en/api/v2/data?id=97'; // mit korrekter API-URL ersetzen
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+const data = await loadData();
+console.log(data); // gibt die Daten der API oder false in der Konsole aus
