2562427bc67e2cea25da9229157dfa05c11c23a1
Chiara-Fhgr <chiara.schweizer@stud.fhgr.ch>
2026-05-06 16:06:55 +0200

    Fonts, Variablen angelegt, Aufbau gestartet

--- js/script.js
+++ js/script.js
@@ -10,3 +10,12 @@ async function loadData() {
 }
 const data = await loadData();
 console.log(data); // gibt die Daten der API oder false in der Konsole aus
+
+
+// Karte einbauen
+var map = L.map('map').setView([46.92771791778397, 8.26013878743277], 7.5);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
